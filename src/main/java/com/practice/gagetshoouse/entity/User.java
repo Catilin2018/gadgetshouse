f7966bb6b15ee4e344c64ec9a5ed01624c5ccfa5
Caitlin <Zonadyan@outlook.com>
2024-12-07 15:47:24 +0000

    Test submmit with User.java

--- src/main/java/com/practice/gagetshoouse/entity/User.java
+++ src/main/java/com/practice/gagetshoouse/entity/User.java
@@ -1,5 +1,76 @@
-package com.practice.gagetshoouse.entity;/**
+package com.practice.gagetshoouse.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+/**
  * @author Deyan
  * @date 2024/12/7
- */public class User {
+ */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String passowrd;
+    private String email;
+    private String role;
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassowrd() {
+        return passowrd;
+    }
+
+    public void setPassowrd(String passowrd) {
+        this.passowrd = passowrd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
 }
