e2da9dbdf9474d51e34766a1837f6839fa58edd2
Caitlin <Zonadyan@outlook.com>
2024-12-07 00:13:01 +0000

    first commit

---
+++ src/main/java/com/practice/gagetshoouse/entity/User.java
@@ -0,0 +1,5 @@
+package com.practice.gagetshoouse.entity;/**
+ * @author Deyan
+ * @date 2024/12/7
+ */public class User {
+}
---
+++ src/test/java/com/practice/gagetshoouse/GagetsHoouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.gagetshoouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GagetsHoouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
