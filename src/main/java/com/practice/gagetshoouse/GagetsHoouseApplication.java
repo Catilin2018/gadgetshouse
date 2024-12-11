f05cf299f73035ac9c0800c26a1746300c363aec
Caitlin <Zonadyan@outlook.com>
2024-12-11 10:56:49 +0000

    delete some files

--- src/main/java/com/practice/gagetshoouse/GagetsHoouseApplication.java
+++ src/main/java/com/practice/gagetshoouse/GagetsHoouseApplication.java
@@ -3,11 +3,12 @@ package com.practice.gagetshoouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.practice.gagetshoouse")
 public class GagetsHoouseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GagetsHoouseApplication.class, args);
+
     }
 
 }
--- src/test/java/com/practice/gagetshoouse/GagetsHoouseApplicationTests.java
+++ src/test/java/com/practice/gagetshoouse/GagetsHoouseApplicationTests.java
@@ -1,6 +1,8 @@
 package com.practice.gagetshoouse;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
